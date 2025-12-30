1c37c885f14b5e4d8f8673fd80f0e3329e5adfed
Chris Meyers <chris@chrismeyers.net>
2025-12-29 19:28:04 -0700

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/chrismeyers/gex
+
+go 1.25.5
---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"encoding/hex"
+	"flag"
+	"fmt"
+	"os"
+	"slices"
+	"strings"
+)
+
+const LINE_LENGTH = 60
+
+func main() {
+	flag.Parse()
+
+	inputFile := flag.Arg(0)
+	if inputFile == "" {
+		fmt.Println("Usage: gex <input file>")
+		os.Exit(1)
+	}
+
+	input, err := os.ReadFile(inputFile)
+	if err != nil {
+		panic(err)
+	}
+
+	displacement := 0
+	for chunk := range slices.Chunk(input, 16) {
+		var line strings.Builder
+		fmt.Fprintf(&line, "%08x  ", displacement)
+
+		var ascii strings.Builder
+		for i := range chunk {
+			if i == 8 {
+				line.WriteString(" ")
+			}
+			fmt.Fprintf(&line, "%02x", chunk[i])
+
+			value, _ := hex.DecodeString(string(fmt.Sprintf("%02x", chunk[i])))
+			str := string(value)
+			switch str {
+			case "\n", "\t":
+				ascii.WriteString(".")
+			default:
+				ascii.WriteString(str)
+			}
+
+			line.WriteString(" ")
+		}
+		if line.Len() < LINE_LENGTH {
+			line.WriteString(strings.Repeat(" ", LINE_LENGTH-line.Len()))
+		}
+		fmt.Fprintf(&line, "|%s|", ascii.String())
+		fmt.Println(line.String())
+		displacement += len(chunk)
+	}
+
+	if (displacement % 16) != 0 {
+		fmt.Printf("%08x\n", displacement)
+	}
+}
