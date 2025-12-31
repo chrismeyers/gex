81d992410c65617b6f333d92e493041ed807afee
Chris Meyers <chris@chrismeyers.net>
2025-12-30 19:03:56 -0700

    Add image input examples

--- test.sh
+++ test.sh
@@ -12,3 +12,18 @@ diff --color=always <(go run . input/repeats.txt) <(hexdump input/repeats.txt)
 diff --color=always <(go run . -C input/repeats.txt) <(hexdump -C input/repeats.txt)
 diff --color=always <(go run . -v input/repeats.txt) <(hexdump -v input/repeats.txt)
 diff --color=always <(go run . -C -v input/repeats.txt) <(hexdump -Cv input/repeats.txt)
+
+diff --color=always <(go run . input/1.webp) <(hexdump input/1.webp)
+diff --color=always <(go run . -C input/1.webp) <(hexdump -C input/1.webp)
+diff --color=always <(go run . -v input/1.webp) <(hexdump -v input/1.webp)
+diff --color=always <(go run . -C -v input/1.webp) <(hexdump -Cv input/1.webp)
+
+diff --color=always <(go run . input/wikipedia.png) <(hexdump input/wikipedia.png)
+diff --color=always <(go run . -C input/wikipedia.png) <(hexdump -C input/wikipedia.png)
+diff --color=always <(go run . -v input/wikipedia.png) <(hexdump -v input/wikipedia.png)
+diff --color=always <(go run . -C -v input/wikipedia.png) <(hexdump -Cv input/wikipedia.png)
+
+diff --color=always <(go run . input/apple.jpg) <(hexdump input/apple.jpg)
+diff --color=always <(go run . -C input/apple.jpg) <(hexdump -C input/apple.jpg)
+diff --color=always <(go run . -v input/apple.jpg) <(hexdump -v input/apple.jpg)
+diff --color=always <(go run . -C -v input/apple.jpg) <(hexdump -Cv input/apple.jpg)
